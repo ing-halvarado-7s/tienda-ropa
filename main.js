22d9db1c215244ebe68963d0ca5c3b628ef081e2
Heimys Alvarado <ing.halvarado.7s@gmail.com>
2020-08-05 22:48:41 -0400

    Add files via upload
    Commit final por hoy

---
+++ main.js
@@ -0,0 +1,49 @@
+new Vue({
+    el: '#app',
+    // vuetify: new Vuetify(),
+    data () {
+      return {
+        url:'https://api.tissini.app',
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        // .get('https://api.tissini.app/api/v1/login/client')
+      // .get('https://api.tissini.app/api/v2/categories/343/products')
+        //.get('https://api.tissini.app/api/v2/categories/1/products')
+        // .get('https://api.tissini.app/api/v1/categories/sections') //Secciones
+         .get('https://api.tissini.app/api/v2/categories') //Categorias
+        .then(response => (this.info = response.data))
+    }
+  });
+
+  Vue.config.devtools = true;
+
+
+
+// var app = new Vue({
+//     el:"#app",
+//     data(){
+//         return{
+//             respuesta: {},
+//         }//fin de return de data
+//     },//fin de data
+//     methods: {
+//         obtenerData(){
+//             // axios.get('https://in.hotjar.com/api/v2/client/sites/1050891/visit-data?sv=6')
+//             axios.get('https://api.tissini.app/api/v2/categories')
+//                 .then(response=>{
+//                     this.respuesta = response.data;
+//                 })
+//                 .catch(error=>{
+//                     console.error(error);
+//                 })
+//         }//Fin de obtenerData()
+//     }, //fin de methods
+//     computed: {
+//         parsedResponse(){
+//             return JSON.stringify(this.respuesta);
+//         }
+//     },
+// })
